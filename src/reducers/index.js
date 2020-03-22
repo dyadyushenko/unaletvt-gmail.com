c5f075edc1c64972f7853c2163dbda8fbda8b409
unale <unaletvt@gmail.com>
2020-03-22 22:48:39 +0300

    create Reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import images from "./imageReducers";
+import videos from "./videoReducers";
+
+const rootReducer = combineReducers({
+    images,
+    videos
+});
+
+export default rootReducer
